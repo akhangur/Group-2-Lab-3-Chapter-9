f1500dc0fab46a49fba06bf672ab950c81041749
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-01 18:02:15 -0500

    Step 4 Project 2- mouse over and mouse out event

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -1,21 +1,34 @@
 /* add code here */
-
-window.addEventListener("load", function()          
+//Step 2
+window.addEventListener("load", function()                                              //Load event
 {
-    var clickImg= document.querySelectorAll("#thumbnails img");
-    for(i=0;i<clickImg.length;i++)
-    {
+    var clickImg= document.querySelector("#thumbnails");
+    var caption = document.querySelector("#featured figcaption");
         //Step 3
-        clickImg[i].addEventListener("click",function(event)                    
+        clickImg.addEventListener("click",function(event)                              //click event
         {
             var filepath = event.target.src;    
-            var filename = filepath.split("images/small");                       
+            var filename = filepath.split("images/small");                              // split till images/small and extract the filename only  
             document.querySelector("#featured img").src = "images/medium"+filename[1];
             document.querySelector("#featured img").title = event.target.title; 
             document.querySelector("#featured img").alt = event.target.alt;
-            var caption = document.querySelector("#featured figcaption");
-            caption.style.opacity = "60%";
             caption.innerHTML = event.target.title;
         })
-    }
+    
+
+    //Step 4
+    var LImg = document.querySelector("#featured img");
+    LImg.addEventListener("mouseover",function(event)                    //mouseover event
+        {
+            caption.style.opacity = "80%";
+        })
+
+
+    LImg.addEventListener("mouseout",function(event)                    //mouseout event
+        {
+            caption.style.opacity = "0%";
+           
+
+        })
+    
 })
