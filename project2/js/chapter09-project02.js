fbac68fa8edc6bf443a05ae202e822cfbd3ae553
akhangur <65823090+akhangur@users.noreply.github.com>
2021-02-01 17:47:50 -0500

    Step 3

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -1,5 +1,21 @@
 /* add code here */
-windows.addEventListener("load",functions()
-                         {
-                         alert("from load");
+
+window.addEventListener("load", function()          
+{
+    var clickImg= document.querySelectorAll("#thumbnails img");
+    for(i=0;i<clickImg.length;i++)
+    {
+        //Step 3
+        clickImg[i].addEventListener("click",function(event)                    
+        {
+            var filepath = event.target.src;    
+            var filename = filepath.split("images/small");                       
+            document.querySelector("#featured img").src = "images/medium"+filename[1];
+            document.querySelector("#featured img").title = event.target.title; 
+            document.querySelector("#featured img").alt = event.target.alt;
+            var caption = document.querySelector("#featured figcaption");
+            caption.style.opacity = "60%";
+            caption.innerHTML = event.target.title;
+        })
+    }
 })
